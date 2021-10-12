1139c8d5930eacf5b283d30c72f49ad67676b39f
franti165 <wowwowcircle2020@gmail.com>
2021-10-12 22:09:13 +0300

    Added ASCII image

---
+++ Lesson 1/task 1/Knigga.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "    _______ " << endl;
+    cout << "   /      /, " << endl;
+    cout << "  /      // " << endl;
+    cout << " /______// " << endl;
+    cout << "(______(/ " << endl;
+    cout << "            Jussi Roine " << endl;
+    return 0;
+}
