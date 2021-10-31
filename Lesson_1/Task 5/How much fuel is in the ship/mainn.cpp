4bc9b5e138c54db17e862ed7505d56319dce7090
franti165 <wowwowcircle2020@gmail.com>
2021-10-31 03:12:31 +0300

    Replaced the old code with the new code, made how much fuel is needed for 300 light flights

---
+++ Lesson_1/Task 5/How much fuel is in the ship/mainn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int fuel;
+    cout << "Enter the mass" << endl;
+    cin >> fuel;
+    cout << (fuel / 3 - 2) * 300 << endl;
+    return 0;
+}
